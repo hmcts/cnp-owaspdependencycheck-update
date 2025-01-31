a735073d956656db6fe1dc2f0489489e11925c87
Hannah Smith <hannah.smith16@justice.gov.uk>
2025-01-31 14:37:33 +0000

    correct - grabbed from initialize_postgres.sql

--- db-migrations/v10/V010__upgrade_5_4.sql
+++ db-migrations/v10/V010__upgrade_5_4.sql
@@ -1,6 +1,6 @@
-DROP PROCEDURE IF EXISTS public.update_vulnerability;
+DROP FUNCTION IF EXISTS public.update_vulnerability;
 
-CREATE PROCEDURE update_vulnerability (
+CREATE FUNCTION update_vulnerability (
     IN p_cveId VARCHAR(20), IN p_description VARCHAR(8000), IN p_v2Severity VARCHAR(20), 
     IN p_v2ExploitabilityScore DECIMAL(3,1), IN p_v2ImpactScore DECIMAL(3,1), IN p_v2AcInsufInfo BOOLEAN, 
     IN p_v2ObtainAllPrivilege BOOLEAN, IN p_v2ObtainUserPrivilege BOOLEAN, IN p_v2ObtainOtherPrivilege BOOLEAN, 
@@ -45,20 +45,25 @@ IF vulnerabilityId > 0 THEN
         v3ExploitabilityScore=p_v3ExploitabilityScore, v3ImpactScore=p_v3ImpactScore, v3AttackVector=p_v3AttackVector, 
         v3AttackComplexity=p_v3AttackComplexity, v3PrivilegesRequired=p_v3PrivilegesRequired, v3UserInteraction=p_v3UserInteraction, 
         v3Scope=p_v3Scope, v3ConfidentialityImpact=p_v3ConfidentialityImpact, v3IntegrityImpact=p_v3IntegrityImpact, 
-        v3AvailabilityImpact=p_v3AvailabilityImpact, v3BaseScore=p_v3BaseScore, v3BaseSeverity=p_v3BaseSeverity, v3Version=p_v3Version, v4version=p_v4version, v4attackVector=p_v4attackVector, v4attackComplexity=p_v4attackComplexity,
-        v4attackRequirements=p_v4attackRequirements, v4privilegesRequired=p_v4privilegesRequired, v4userInteraction=p_v4userInteraction,
-        v4vulnConfidentialityImpact=p_v4vulnConfidentialityImpact, v4vulnIntegrityImpact=p_v4vulnIntegrityImpact, v4vulnAvailabilityImpact=p_v4vulnAvailabilityImpact,
-        v4subConfidentialityImpact=p_v4subConfidentialityImpact, v4subIntegrityImpact=p_v4subIntegrityImpact,
-        v4subAvailabilityImpact=p_v4subAvailabilityImpact, v4exploitMaturity=p_v4exploitMaturity, v4confidentialityRequirement=p_v4confidentialityRequirement,
-        v4integrityRequirement=p_v4integrityRequirement, v4availabilityRequirement=p_v4availabilityRequirement, v4modifiedAttackVector=p_v4modifiedAttackVector,
-        v4modifiedAttackComplexity=p_v4modifiedAttackComplexity, v4modifiedAttackRequirements=p_v4modifiedAttackRequirements, v4modifiedPrivilegesRequired=p_v4modifiedPrivilegesRequired,
-        v4modifiedUserInteraction=p_v4modifiedUserInteraction, v4modifiedVulnConfidentialityImpact=p_v4modifiedVulnConfidentialityImpact, v4modifiedVulnIntegrityImpact=p_v4modifiedVulnIntegrityImpact,
-        v4modifiedVulnAvailabilityImpact=p_v4modifiedVulnAvailabilityImpact, v4modifiedSubConfidentialityImpact=p_v4modifiedSubConfidentialityImpact, v4modifiedSubIntegrityImpact=p_v4modifiedSubIntegrityImpact,
-        v4modifiedSubAvailabilityImpact=p_v4modifiedSubAvailabilityImpact, v4safety=p_v4safety, v4automatable=p_v4automatable, v4recovery=p_v4recovery,
-        v4valueDensity=p_v4valueDensity, v4vulnerabilityResponseEffort=p_v4vulnerabilityResponseEffort, v4providerUrgency=p_v4providerUrgency,
-        v4baseScore=p_v4baseScore, v4baseSeverity=p_v4baseSeverity, v4threatScore=p_v4threatScore, v4threatSeverity=p_v4threatSeverity,
-        v4environmentalScore=p_v4environmentalScore, v4environmentalSeverity=p_v4environmentalSeverity, v4source=p_v4source, v4type=p_v4type);
-
+        v3AvailabilityImpact=p_v3AvailabilityImpact, v3BaseScore=p_v3BaseScore, v3BaseSeverity=p_v3BaseSeverity, v3Version=p_v3Version,
+        v4version=p_v4version, v4attackVector=p_v4attackVector, v4attackComplexity=p_v4attackComplexity, 
+        v4attackRequirements=p_v4attackRequirements, v4privilegesRequired=p_v4privilegesRequired, 
+        v4userInteraction=p_v4userInteraction, v4vulnConfidentialityImpact=p_v4vulnConfidentialityImpact, 
+        v4vulnIntegrityImpact=p_v4vulnIntegrityImpact, v4vulnAvailabilityImpact=p_v4vulnAvailabilityImpact, 
+        v4subConfidentialityImpact=p_v4subConfidentialityImpact, v4subIntegrityImpact=p_v4subIntegrityImpact, 
+        v4subAvailabilityImpact=p_v4subAvailabilityImpact, v4exploitMaturity=p_v4exploitMaturity, 
+        v4confidentialityRequirement=p_v4confidentialityRequirement, v4integrityRequirement=p_v4integrityRequirement, 
+        v4availabilityRequirement=p_v4availabilityRequirement, v4modifiedAttackVector=p_v4modifiedAttackVector, 
+        v4modifiedAttackComplexity=p_v4modifiedAttackComplexity, v4modifiedAttackRequirements=p_v4modifiedAttackRequirements, 
+        v4modifiedPrivilegesRequired=p_v4modifiedPrivilegesRequired, v4modifiedUserInteraction=p_v4modifiedUserInteraction, 
+        v4modifiedVulnConfidentialityImpact=p_v4modifiedVulnConfidentialityImpact, v4modifiedVulnIntegrityImpact=p_v4modifiedVulnIntegrityImpact, 
+        v4modifiedVulnAvailabilityImpact=p_v4modifiedVulnAvailabilityImpact, v4modifiedSubConfidentialityImpact=p_v4modifiedSubConfidentialityImpact, 
+        v4modifiedSubIntegrityImpact=p_v4modifiedSubIntegrityImpact, v4modifiedSubAvailabilityImpact=p_v4modifiedSubAvailabilityImpact, 
+        v4safety=p_v4safety, v4automatable=p_v4automatable, v4recovery=p_v4recovery, v4valueDensity=p_v4valueDensity, 
+        v4vulnerabilityResponseEffort=p_v4vulnerabilityResponseEffort, v4providerUrgency=p_v4providerUrgency, 
+        v4baseScore=p_v4baseScore, v4baseSeverity=p_v4baseSeverity, v4threatScore=p_v4threatScore, 
+        v4threatSeverity=p_v4threatSeverity, v4environmentalScore=p_v4environmentalScore, v4environmentalSeverity=p_v4environmentalSeverity,
+        v4source=p_v4source, v4type=p_v4type
     WHERE id=vulnerabilityId;
 ELSE
     INSERT INTO vulnerability (cve, description, 
@@ -71,20 +76,17 @@ ELSE
         v3ImpactScore, v3AttackVector, v3AttackComplexity, 
         v3PrivilegesRequired, v3UserInteraction, v3Scope, 
         v3ConfidentialityImpact, v3IntegrityImpact, v3AvailabilityImpact, 
-        v3BaseScore, v3BaseSeverity, v3Version, v4version, v4attackVector, 
-        v4attackComplexity, v4attackRequirements, v4privilegesRequired, v4userInteraction, 
-        v4vulnConfidentialityImpact, v4vulnIntegrityImpact, v4vulnAvailabilityImpact, 
-        v4subConfidentialityImpact, v4subIntegrityImpact, v4subAvailabilityImpact, 
-        v4exploitMaturity, v4confidentialityRequirement, v4integrityRequirement, 
-        v4availabilityRequirement, v4modifiedAttackVector, v4modifiedAttackComplexity, 
-        v4modifiedAttackRequirements, v4modifiedPrivilegesRequired, v4modifiedUserInteraction, 
-        v4modifiedVulnConfidentialityImpact, v4modifiedVulnIntegrityImpact, 
-        v4modifiedVulnAvailabilityImpact, v4modifiedSubConfidentialityImpact, 
-        v4modifiedSubIntegrityImpact, v4modifiedSubAvailabilityImpact, v4safety, 
-        v4automatable, v4recovery, v4valueDensity, v4vulnerabilityResponseEffort, 
-        v4providerUrgency, v4baseScore, v4baseSeverity, v4threatScore, 
-        v4threatSeverity, v4environmentalScore, v4environmentalSeverity,
-        v4source, v4type) 
+        v3BaseScore, v3BaseSeverity, v3Version, v4version, 
+        v4attackVector, v4attackComplexity, v4attackRequirements, v4privilegesRequired, 
+        v4userInteraction, v4vulnConfidentialityImpact, v4vulnIntegrityImpact, v4vulnAvailabilityImpact, 
+        v4subConfidentialityImpact, v4subIntegrityImpact, v4subAvailabilityImpact, v4exploitMaturity, 
+        v4confidentialityRequirement, v4integrityRequirement, v4availabilityRequirement, 
+        v4modifiedAttackVector, v4modifiedAttackComplexity, v4modifiedAttackRequirements, 
+        v4modifiedPrivilegesRequired, v4modifiedUserInteraction, v4modifiedVulnConfidentialityImpact, 
+        v4modifiedVulnIntegrityImpact, v4modifiedVulnAvailabilityImpact, v4modifiedSubConfidentialityImpact, 
+        v4modifiedSubIntegrityImpact, v4modifiedSubAvailabilityImpact, v4safety, v4automatable, v4recovery, 
+        v4valueDensity, v4vulnerabilityResponseEffort, v4providerUrgency, v4baseScore, v4baseSeverity, 
+        v4threatScore, v4threatSeverity, v4environmentalScore, v4environmentalSeverity, v4source, v4type) 
         VALUES (p_cveId, p_description, 
         p_v2Severity, p_v2ExploitabilityScore, 
         p_v2ImpactScore, p_v2AcInsufInfo, p_v2ObtainAllPrivilege, 
@@ -95,7 +97,8 @@ ELSE
         p_v3ImpactScore, p_v3AttackVector, p_v3AttackComplexity, 
         p_v3PrivilegesRequired, p_v3UserInteraction, p_v3Scope, 
         p_v3ConfidentialityImpact, p_v3IntegrityImpact, p_v3AvailabilityImpact, 
-        p_v3BaseScore, p_v3BaseSeverity, p_v3Version, p_v4version, p_v4attackVector, p_v4attackComplexity, p_v4attackRequirements, p_v4privilegesRequired, 
+        p_v3BaseScore, p_v3BaseSeverity, p_v3Version, p_v4version, 
+        p_v4attackVector, p_v4attackComplexity, p_v4attackRequirements, p_v4privilegesRequired, 
         p_v4userInteraction, p_v4vulnConfidentialityImpact, p_v4vulnIntegrityImpact, p_v4vulnAvailabilityImpact, 
         p_v4subConfidentialityImpact, p_v4subIntegrityImpact, p_v4subAvailabilityImpact, p_v4exploitMaturity, 
         p_v4confidentialityRequirement, p_v4integrityRequirement, p_v4availabilityRequirement, 
@@ -104,8 +107,7 @@ ELSE
         p_v4modifiedVulnIntegrityImpact, p_v4modifiedVulnAvailabilityImpact, p_v4modifiedSubConfidentialityImpact, 
         p_v4modifiedSubIntegrityImpact, p_v4modifiedSubAvailabilityImpact, p_v4safety, p_v4automatable, p_v4recovery, 
         p_v4valueDensity, p_v4vulnerabilityResponseEffort, p_v4providerUrgency, p_v4baseScore, p_v4baseSeverity, 
-        p_v4threatScore, p_v4threatSeverity, p_v4environmentalScore, p_v4environmentalSeverity,
-        p_v4source, p_v4type);
+        p_v4threatScore, p_v4threatSeverity, p_v4environmentalScore, p_v4environmentalSeverity, p_v4source, p_v4type);
         
     SELECT lastval() INTO vulnerabilityId;
 END IF;
@@ -114,4 +116,16 @@ RETURN QUERY SELECT vulnerabilityId;
 END;
 $$ LANGUAGE plpgsql;
 
-GRANT EXECUTE ON PROCEDURE public.update_vulnerability TO 'dcuser';
+GRANT EXECUTE ON FUNCTION public.update_vulnerability(VARCHAR(20), VARCHAR(8000), 
+    VARCHAR(20), DECIMAL(3,1), DECIMAL(3,1), BOOLEAN, BOOLEAN, BOOLEAN, BOOLEAN, 
+    BOOLEAN, DECIMAL(3,1), VARCHAR(20), VARCHAR(20), VARCHAR(20), VARCHAR(20), 
+    VARCHAR(20), VARCHAR(20), VARCHAR(5), DECIMAL(3,1), DECIMAL(3,1), VARCHAR(20), 
+    VARCHAR(20), VARCHAR(20), VARCHAR(20), VARCHAR(20), VARCHAR(20), VARCHAR(20), 
+    VARCHAR(20), DECIMAL(3,1), VARCHAR(20), VARCHAR(5), VARCHAR(5), VARCHAR(15), 
+    VARCHAR(15), VARCHAR(15), VARCHAR(15), VARCHAR(15), VARCHAR(15), VARCHAR(15), 
+    VARCHAR(15), VARCHAR(15), VARCHAR(15), VARCHAR(15), VARCHAR(20), VARCHAR(15), 
+    VARCHAR(15), VARCHAR(15), VARCHAR(15), VARCHAR(15), VARCHAR(15), VARCHAR(15), 
+    VARCHAR(15), VARCHAR(15), VARCHAR(15), VARCHAR(15), VARCHAR(15), VARCHAR(15), 
+    VARCHAR(15), VARCHAR(15), VARCHAR(15), VARCHAR(15), VARCHAR(15), VARCHAR(15), 
+    VARCHAR(15), DECIMAL(3,1), VARCHAR(15), DECIMAL(3,1), VARCHAR(15), DECIMAL(3,1), 
+    VARCHAR(15), VARCHAR(50), VARCHAR(15)) TO dcuser;
